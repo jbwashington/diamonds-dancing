2ee3d93b5342d3d44f01b271c9e56af849eabb45
James Washington <jbwashington@gmail.com>
2016-08-11 23:59:29 -0500

    restructured sass

--- js/hover.js
+++
@@ -1,9 +0,0 @@
-$('a.projects').hover(function(){
-  $(this).children().stop().eq(0).animate({
-    opacity: 0
-  }).next().fadeIn();
-}, function(){
-  $(this).children().stop().eq(1).fadeOut().prev().animate({
-    opacity: 1
-  });
-});
